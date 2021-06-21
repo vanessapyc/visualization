5d45e8a34ac1def5f2d6eae35dafed71b03aa573
vanessapyc <85813303+vanessapyc@users.noreply.github.com>
2021-06-21 15:00:02 -0700

    Removed parse_args function and some comments

--- functions.py
+++ functions.py
@@ -1,23 +1,8 @@
 import pandas as pd
-import argparse
 import jinja2
 from plotly.subplots import make_subplots
 from plotly import graph_objects as go
 import math
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description='Takes data from QC and creates stacked bar plots')
-    parser.add_argument('--inputfile', type=str,
-                        default='~/Desktop/datafile.csv',
-                        help='Input file name, including directory')
-    parser.add_argument('--htmlfile', type=str, default='QC_data_report.html',
-                        help='File name for HTML file')
-    return parser.parse_args()
-
-
-args = parse_args()
 
 
 def read_data(df1):
@@ -36,13 +21,13 @@ def read_data(df1):
     return df_new
 
 
-def create_html_table(df4):
+def create_html_table(df2):
     def table_colour(val):
         if val:
             colour = 'black'
             return 'colour: %s' % colour
 
-    styler = df4.style.applymap(table_colour)
+    styler = df2.style.applymap(table_colour)
 
     # Template handling
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=''))
@@ -53,23 +38,7 @@ def create_html_table(df4):
     return html
 
 
-def write_html_file(df, fig, html_template):
-    header = "<h1>QC Visualization Report</h1>"
-    body = "This is a dehosted QC report for %i samples.<br><br> Note: " \
-           "Hover over each bar to see the sample names and the exact " \
-           "percentages. <br>" \
-           "To remove a variable, click on the variable name in the " \
-           "legend. <br>" \
-           "To only show one variable, double click on the variable name " \
-           "in the legend." % len(df.index)
-    with open(args.htmlfile, 'w') as f:
-        f.write(header)
-        f.write(body)
-        f.write(fig.to_html())
-        f.write(html_template)
-
-
-def create_subplots(df5):
+def create_subplots(df3):
     # define variables
     limit = 50
     start = 0
@@ -78,24 +47,25 @@ def create_subplots(df5):
     lnd = True
     loop = True
 
-    # number of rows (one graph per row)
-    if len(df5.index) % limit <= 25 and len(df5.index) > limit:
-        calc_rows = int((len(df5.index)) / limit)
+    # determine number of rows (one graph per row)
+    if len(df3.index) % limit <= 25 and len(df3.index) > limit:
+        calc_rows = int((len(df3.index)) / limit)
     else:
-        calc_rows = math.ceil((len(df5.index)) / limit)
+        calc_rows = math.ceil((len(df3.index)) / limit)
 
     fig = make_subplots(rows=calc_rows, cols=1)
 
+    # add traces
     while loop:
         if (row_num + 1) > calc_rows:
             loop = False
-            end += len(df5.index) % limit
+            end += len(df3.index) % limit
 
         fig.add_trace(
             go.Bar(
                 name='Percent human reads filtered',
-                x=df5[start:end]["Sample"],
-                y=df5[start:end]['Percent human reads filtered'],
+                x=df3[start:end]["Sample"],
+                y=df3[start:end]['Percent human reads filtered'],
                 legendgroup='group1',
                 showlegend=lnd,
                 marker=dict(color="#FF934F")
@@ -107,8 +77,8 @@ def create_subplots(df5):
         fig.add_trace(
             go.Bar(
                 name='Percent poor quality reads filtered',
-                x=df5[start:end]["Sample"],
-                y=df5[start:end]['Percent poor quality reads filtered'],
+                x=df3[start:end]["Sample"],
+                y=df3[start:end]['Percent poor quality reads filtered'],
                 legendgroup='group2',
                 showlegend=lnd,
                 marker=dict(color='#CC2D35')
@@ -120,8 +90,8 @@ def create_subplots(df5):
         fig.add_trace(
             go.Bar(
                 name='Percent paired reads kept',
-                x=df5[start:end]["Sample"],
-                y=df5[start:end]['Percent paired reads kept'],
+                x=df3[start:end]["Sample"],
+                y=df3[start:end]['Percent paired reads kept'],
                 legendgroup='group3',
                 showlegend=lnd,
                 marker=dict(color='#058ED9')
@@ -129,24 +99,20 @@ def create_subplots(df5):
             row=row_num,
             col=1,
         )
-        # text=df5['Sample'],
-        # textposition='auto',
-        # textfont=dict(color='black', size=15)
-
-        lnd = False
 
         fig.update_xaxes(title_text='<b>Sample</b>', row=row_num, col=1,
                          tickmode='linear', tickfont=dict(size=10),
                          tickangle=90)
-        # tickmode = 'linear', tickfont = dict(size=10), tickangle=90
-        # showticklabels=False, tickcolor='white'
+
         fig.update_yaxes(title_text='<b>Percent</b>', row=row_num, col=1,
                          dtick=10)
 
+        lnd = False
         row_num += 1
         start += limit
         end += limit
 
+    # update figure properties
     fig.update_layout(barmode='stack',
                       legend_title='',
                       template='simple_white',
@@ -164,3 +130,19 @@ def create_subplots(df5):
                       )
 
     return fig
+
+
+def write_html_file(args, df4, fig, html_template):
+    header = "<h1>QC Visualization Report</h1>"
+    body = "This is a dehosted QC report for %i samples.<br><br> Note: " \
+           "Hover over each bar to see the sample names and the exact " \
+           "percentages. <br>" \
+           "To remove a variable, click on the variable name in the " \
+           "legend. <br>" \
+           "To only show one variable, double click on the variable name " \
+           "in the legend." % len(df4.index)
+    with open(args.htmlfile, 'w') as f:
+        f.write(header)
+        f.write(body)
+        f.write(fig.to_html())
+        f.write(html_template)
